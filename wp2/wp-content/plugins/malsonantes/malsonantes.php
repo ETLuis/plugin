dc85337b284cb4c7b9d0d5b677520fc730fd9703
Jorge y Luis <lcampanogoldar@gmail.com>
2022-11-20 19:53:54 +0100

    Plugin con base de datos.

--- wp2/wp-content/plugins/malsonantes/malsonantes.php
+++ wp2/wp-content/plugins/malsonantes/malsonantes.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection LanguageDetectionInspection */
 
 /*
 Plugin Name: Cambiar Malsonantes
@@ -11,11 +11,10 @@ Author URI: https://twitter.com/LuisGoldar
 
 
 function cambiar_malsonantes($text) {
-    $malsonante = array(caca, joder, hostia, mierda, gilipollas);
+    $malsonante = array('caca', 'joder', 'hostia', 'mierda', 'gilipollas');
     $palabra = str_replace($malsonante, "popo", $text);
 
     return $palabra;
-
 }
 
 
@@ -23,3 +22,27 @@ function cambiar_malsonantes($text) {
  * cambia el contenido del host
  */
 add_filter('the_content', 'cambiar_malsonantes');
+
+
+function cambiar_malsonantes_base_de_datos() {
+    // Creamos un objeto del WordPress para trabajar con la BD llamado wpdb.
+    global $wpdb;
+
+    // Recojemos el charset.
+    $charset = $wpdb->get_charset_collate();
+
+    // Le añado el prefijo a la tabla.
+    $table_name = $wpdb->prefix . 'wp';
+
+    // Creo una tabla y le añado un campo llamado "palabras".
+    $sql = "CREATE TABLE $table_name (
+        palabras text NOT NULL,
+        PRIMARY KEY (palabras)
+    ) $charset;";
+
+    // libreria necesaria para usar la funcion dbDelta.
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta( $sql );
+}
+
+add_action( 'plugins_loaded', 'cambiar_malsonantes_base_de_datos' );
